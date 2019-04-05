4a9babd74d79612c8627b10fecb7bedd712ee9b7
Sirataki <yff07512@gmail.com>
2019-04-05 13:16:45 +0900

    Add

---
+++ Assets/Scripts/JoystickInput.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class JoystickInput : MonoBehaviour
+{
+	[SerializeField] private float radius;
+	[SerializeField] private int threshold;
+	[SerializeField] private RectTransform handle = null;
+	[SerializeField] private JoystickInputCollider[] inputCollider = new JoystickInputCollider[0];
+
+	protected int m_Cost;
+
+	public bool IsSpin
+	{
+		get { return m_Cost > threshold; }
+	}
+
+	void Start()
+	{
+		Vector2 center = new Vector2(0.5f, 0.5f);
+		handle.anchorMin = center;
+		handle.anchorMax = center;
+		handle.pivot = center;
+		handle.anchoredPosition = Vector2.zero;
+	}
+
+	void Update()
+	{
+		handle.anchoredPosition = InputAxis() * radius;
+
+		m_Cost = 0;
+		for (int i = 0; i < inputCollider.Length; ++i)
+		{
+			m_Cost += inputCollider[i].Cost;
+		}
+	}
+
+	private Vector2 InputAxis()
+	{
+		Vector2 axis = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		return axis;
+	}
+}
---
+++ Assets/Scripts/JoystickInputCollider.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+
+public class JoystickInputCollider : MonoBehaviour
+{
+	protected int value;		//InActive=0,Active=1
+
+	public int Cost
+	{
+		get { return value; }
+	}
+
+	WaitForSeconds m_Wait;
+
+	Coroutine m_WaitCoroutine;
+
+	const float k_InputDuration = 0.3f;
+
+	void Awake()
+	{
+		m_Wait = new WaitForSeconds(k_InputDuration);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (m_WaitCoroutine != null)
+		{
+			StopCoroutine(m_WaitCoroutine);
+		}
+		m_WaitCoroutine = StartCoroutine(InputWait());
+	}
+
+	IEnumerator InputWait()
+	{
+		value = 1;
+
+		yield return m_Wait;
+
+		value = 0;
+	}
+}
